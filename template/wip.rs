c0349be78247e59221ccc8638e7b905941cb6fcd
Zachery Hostens <zacheryph@gmail.com>
2021-12-10 22:47:10 -0600

    add template cause i don't like the `aoc` default

---
+++ template/wip.rs
@@ -0,0 +1,21 @@
+const INPUT: &str = include_str!("input.txt");
+// const INPUT: &str = include_str!("test.txt");
+
+fn parse_line(line: &str) -> usize {
+    0
+}
+
+fn stage_one() -> i64 {
+    0
+}
+
+fn stage_two() -> i64 {
+    0
+}
+
+fn main() {
+    // Stage 1:
+    println!("Stage 1: {}", stage_one());
+    // Stage 2:
+    println!("Stage 2: {}", stage_two());
+}
