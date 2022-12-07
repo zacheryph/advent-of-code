d3d9c30ddc05d280e0e04a04202b0596f60e05eb
Zachery Hostens <zacheryph@gmail.com>
2022-12-07 13:41:01 -0600

    remove useless comment

--- ruby/challenges/2022/06/solution.rb
+++ ruby/challenges/2022/06/solution.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 module Year2022
   class Day06 < Solution
-    # @input is available if you need the raw data input
-    # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
-
     def part_1
       start_of_packet(4)
     end
